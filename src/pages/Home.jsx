74c4a1e9670c5f6350b0488e85d3ac92019d6a83
San <sanvu2001@gmail.com>
2025-04-08 16:41:24 -0700

    add image to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react"
 
+
 export default function Home() {
     return (
         <div className="home-container">
             <h2>Welcome to</h2>
             <div className="home-logo">yarnies</div>
             <p>Keep track of your bajillion WIPs and all the yarn you keep buying.</p>
+            <br /><br />
+            <img src="src/assets/projects-screenshot.png" />
         </div>
     )
 };
